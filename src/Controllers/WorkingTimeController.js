28ab89cbe4844681f2b231c19e6d4dfb076a6534
Selina Frenes <stfresel@bx.fallmerayer.it>
2024-05-05 15:42:18 +0200

    added new Endpoint wip

--- src/Controllers/WorkingTimeController.js
+++ src/Controllers/WorkingTimeController.js
@@ -1,12 +1,16 @@
 const {getTotalWorkingTime} = require("../Models/db_access");
+// TODO yxc kommi
 const WorkingTimeController = (req, res) => {
     getTotalWorkingTime()
         .then(response => {
             console.log("RESPONSE: " + JSON.stringify(response));
             res.json({response});
-                // res.json({ success: true, data: response});
-            // res.status(401).json({ success: false, message: 'Unautorisiert: Benutzername oder Passwort falsch' });
-
+            const r = JSON.parse(response);
+            let time = 0;
+            for (let i = 0; i < r.length; i++) {
+                time += r[i].TotalDuration;
+            }
+            res.status(200).json({totalTime: time})
         })
         .catch((e) => {
             res.status(500).json({ success: false, message: e.message });
