83df0acb9205bc718a915ee893898d4dbbfb1a5a
Selina Frenes <stfresel@bx.fallmerayer.it>
2024-05-05 14:03:41 +0200

    db-daten übersichtlicher darstellen

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 
 /**
  * Eine Tabelle zum Anzeigen von Tagebucheinträgen.
@@ -24,42 +24,50 @@ function Table(props){
  * @returns {JSX.Element} Die gerenderte Tabelle.
  */
 const filterTableByUsername = (data) => {
-    // Rendert JSX-Tabelle mit gruppierten und sortierten Einträgen
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [isExpanded, setIsExpanded] = useState("");
+
+    const toggleExpand = (username) => {
+        setIsExpanded(username === isExpanded ? "" : username);
+    };
+
     return (
         data.data.map(user => (
             <table className="output-filed-table">
                 <Fragment key={user.username}>
-                    {
-                        <thead className="output-filed-table-thead">
-                        <th className="tableHeader" colSpan={4}>
-                            {user.username}
-                        </th>
+                    <thead className="output-filed-table-thead">
+                    <th className="tableHeader" colSpan={4} onClick={() => toggleExpand(user.username)}>
+                        {user.username}
+                    </th>
+                    {isExpanded === user.username && (
                         <tr>
                             <th>Datum</th>
                             <th>Zeit</th>
                             <th>Beschreibung</th>
                             <th>Notizen</th>
                         </tr>
-                        </thead>
-                    }
+                    )}
+                    </thead>
 
-                    {<tbody className="output-filed-table-tbody">
-                    {JSON.parse(user.eintraege) !== null &&
-                        JSON.parse(user.eintraege).map((entry) => (
-                            <tr key={"_" + entry.id + "_" + entry.date + "_" + entry.duration}>
-                                <td>{new Date(entry.date).toLocaleDateString('de-DE')}</td>
-                                <td>{entry.duration}</td>
-                                <td>{entry.description.replaceAll(/'/g, '').replaceAll(/\\n/g, ' ')}</td>
-                                <td>{entry.notes.replaceAll(/'/g, '').replaceAll(/\\n/g, ' ')}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                    }
+                    {isExpanded === user.username && (
+                        <tbody className="output-filed-table-tbody">
+                        {JSON.parse(user.eintraege) !== null &&
+                            JSON.parse(user.eintraege).map((entry) => (
+                                <tr key={"_" + entry.id + "_" + entry.date + "_" + entry.duration}>
+                                    <td>{new Date(entry.date).toLocaleDateString('de-DE')}</td>
+                                    <td>{entry.duration}</td>
+                                    <td>{entry.description.replaceAll(/'/g, '').replaceAll(/\\n/g, ' ')}</td>
+                                    <td>{entry.notes.replaceAll(/'/g, '').replaceAll(/\\n/g, ' ')}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    )}
                 </Fragment>
             </table>
         ))
-    )
-    };
+    );
+};
 
 
 
@@ -74,34 +82,48 @@ const filterTableByUsername = (data) => {
  */
 const filterTableByDate = (data) => {
     // Rendert JSX-Tabelle mit gruppierten und sortierten Einträgen
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [isExpanded, setIsExpanded] = useState("");
+
+    const toggleExpand = (date) => {
+        setIsExpanded(date === isExpanded ? "" : date);
+    };
     return (
             data.data.map(e => (
             <table className="output-filed-table">
                 <Fragment key={new Date(e.date).toLocaleDateString('de-DE')}>
                     {
                         <thead className="output-filed-table-thead">
-                        <th className="tableHeader" colSpan={4}>
+                        <th className="tableHeader" colSpan={4} onClick={() => toggleExpand(e.date)}>
                             {new Date(e.date).toLocaleDateString('de-DE')}
                         </th>
-                        <tr>
-                            <th>Person</th>
-                            <th>Zeit</th>
-                            <th>Beschreibung</th>
-                            <th>Notizen</th>
-                        </tr>
+                        {isExpanded === e.date && (
+                            <tr>
+                                <th>Person</th>
+                                <th>Zeit</th>
+                                <th>Beschreibung</th>
+                                <th>Notizen</th>
+                            </tr>
+                        )}
+
                         </thead>
 
                     }
-                    <tbody className="output-filed-table-tbody">
-                    {JSON.parse(e.eintraege).map((entry) => (
-                        <tr key={"_" + entry.person + "_" + entry.id + "_" + entry.duration}>
-                            <td>{entry.person}</td>
-                            <td>{entry.duration}</td>
-                            <td>{entry.description.replaceAll(/'/g, '').replaceAll(/\\n/g, ' ')}</td>
-                            <td>{entry.notes.replaceAll(/'/g, '').replaceAll(/\\n/g, ' ')}</td>
-                        </tr>
-                    ))}
-                    </tbody>
+
+                    {isExpanded === e.date && (
+                        <tbody className="output-filed-table-tbody">
+                        {JSON.parse(e.eintraege).map((entry) => (
+                                <tr key={"_" + entry.person + "_" + entry.id + "_" + entry.duration}>
+                                    <td>{entry.person}</td>
+                                    <td>{entry.duration}</td>
+                                    <td>{entry.description.replaceAll(/'/g, '').replaceAll(/\\n/g, ' ')}</td>
+                                    <td>{entry.notes.replaceAll(/'/g, '').replaceAll(/\\n/g, ' ')}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    )}
+
                 </Fragment>
             </table>
             ))
