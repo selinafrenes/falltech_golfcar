ec7408c849c8012c8439443ee711635d7f48227a
Selina Frenes <stfresel@bx.fallmerayer.it>
2024-05-08 15:08:05 +0200

    implemented controls website

--- src/Pages/Controls.jsx
+++ src/Pages/Controls.jsx
@@ -19,6 +19,7 @@ const ControlsPage = () => {
         <div>
             {controlsHtml && (
                 <iframe
+                    id="controlsFrame"
                     title="Controls"
                     width="100%"
                     height="100%"
